c445822ba50b0a1eb8ae22281a7846ed5c4c058a
Michele Beltrame <mb@italpro.net>
2022-08-25 16:47:44 +0200

    works!

--- lib/Net/MBE.pm
+++ lib/Net/MBE.pm
@@ -1,19 +1,14 @@
 package Net::MBE {
     use Moo;
     use namespace::clean;
-    use SOAP::Lite +trace => [ qw/all -objects/ ];
+    use SOAP::Lite;
+    #use SOAP::Lite +trace => [ qw/all -objects/ ];
     use MIME::Base64;
     use HTTP::Headers;
     use Arthas::Defaults::536;
 
 	#$SOAP::Constants::DEFAULT_HTTP_CONTENT_TYPE = 'text/xml';
 	#$SOAP::Constants::DO_NOT_USE_CHARSET = 1;
-
-    #sub LWP::UserAgent::get_basic_credentials {
-    sub SOAP::Transport::HTTP::Client::get_basic_credentials {
-        #return 'NEGOZIOCOLTELLINAI@Italy' => 'kGEJ5HzzwjkeMi8pudpLaWD7fX7dn4gZ';
-        warn "\n\nAAAA: @_\n";
-    }
 
 	has system => ( is => 'rw' );
 	has endpoint => ( is => 'rw' );
@@ -69,11 +64,11 @@ package Net::MBE {
         $soapClient->ns('http://www.onlinembe.eu/ws/','ns1');
         $soapClient->envprefix('SOAP-ENV');
         $soapClient->autotype(0);
-		#$soapClient->register_ns('','xsd');
 
         $class->soapClient($soapClient);
         $class->credentials({'Username' => '', Passphrase => ''});
 	}
+
 
 	# ShippingOptionsRequest
 	# ----------------------
@@ -82,9 +77,7 @@ package Net::MBE {
 	#	$shippingParameters		->	object of type ShippingParameters with all
 	#									the required info.
 	sub ShippingOptions($self, $args) {
-		# $internalReferenceID,
-		# $shippingParameters,
-		croak 'Invalid-InternalReferenceID' if !$args->{InternalReferenceID};
+		croak 'Invalid-internalReferenceID' if !$args->{internalReferenceID};
 
         my $params = SOAP::Data->name('RequestContainer' => \SOAP::Data->value(
             SOAP::Data->name('System' => $self->system),
@@ -92,431 +85,15 @@ package Net::MBE {
                 SOAP::Data->name('Username' => ''),
                 SOAP::Data->name('Passphrase' => ''),
             )),
-            SOAP::Data->name('InternalReferenceID', $args->{InternalReferenceID}),
-            SOAP::Data->name('ShippingParameters' => \SOAP::Data->value(
-                SOAP::Data->name('DestinationInfo' => \SOAP::Data->value(
-                    SOAP::Data->name('ZipCode' => '33085'),
-                    SOAP::Data->name('Country' => 'IT'),
-                )),
-                SOAP::Data->name('ShipType', 'EXPORT'),
-                SOAP::Data->name('PackageType', 'GENERIC'),
-                SOAP::Data->name('Items' => \SOAP::Data->value(
-                    SOAP::Data->name('Item' =>  \SOAP::Data->value(
-                        SOAP::Data->name('Weight', '1'),
-                        SOAP::Data->name('Dimensions' =>  \SOAP::Data->value(
-                            SOAP::Data->name('Lenght', '10'),
-                            SOAP::Data->name('Height', '10'),
-                            SOAP::Data->name('Width', '10'),
-                        )),
-                    )),
-                )),
-            )),
+            SOAP::Data->name('InternalReferenceID', $args->{internalReferenceID}),
+			SOAP::Data->name('ShippingParameters' => $args->{shippingParameters}->getSoapParams()),
         ));
 
-		# my $params = array(
-		# 	'RequestContainer' =>array(
-		# 		'System' => $this->system,
-		# 		'Credentials' => $this->credentials,
-		# 		'InternalReferenceID' => $internalReferenceID,
-		# 		'ShippingParameters' => $shippingParameters->getParams()));
-
         my $som = $self->soapClient->call("ShippingOptionsRequest", $params);
-        use Data::Dump qw/dump/; die dump($som);
-        # die $som->fault->{ faultstring } if ($som->fault);
-        # print $som->result, "\n";
-
-
-		# $response = 
-		# 	$this->soapClient
-		# 		->ShippingOptionsRequest($params);
-
-		# if ($debug) {
-		# 	return 
-		# 		'Request : ' . 
-		# 		htmlentities($this->soapClient->__getLastRequest()) . 
-		# 		'Response: <br/>' . 
-		# 		var_dump($response->RequestContainer);
-		# } else {
-		# 	return $response->RequestContainer;
-		# }
-
+		croak 'Invalid-request' if !$som;
+    	croak 'Request error: '.$som->fault->{ faultstring } if $som->fault;
+		return $som->body->{ShippingOptionsRequestResponse}->{RequestContainer};
 	}
-=pod 
-
-	// SOAP Operations
-
-	// AddAtachmentRequest
-	// -------------------
-	// Attaches one or more files to a shipment.
-	//	$internalReferenceId	->	string that will be returned as is
-	//	$MOS					->	MBE shipment tracking number to which
-	//									attach files.
-	//	$filenames				->	a string or an array of string containing
-	//									the names of the files to be attached
-	public function AddAttachment(
-		$internalReferenceId,
-		$MOS,
-		$filenames,
-		$debug = false)
-	{
-		$request_container = array();
-		$request_container[] = new SoapVar($this->credentials, SOAP_ENC_OBJECT, null, null, 'Credentials');
-		$request_container[] = new SoapVar($internalReferenceId, XSD_STRING, null, null, 'InternalReferenceID');
-		$request_container[] = new SoapVar($MOS, XSD_STRING, null, null, 'MasterTrackingMBE');
-
-		if (!is_array($filenames)) $filenames = array($filenames);
-
-		foreach($filenames as $filename) {
-			$file = fopen($filename, "rb");
-			$filedata = fread($file, filesize($filename));
-			if ($filedata === false) {
-				return false;
-			} else {
-				$attachment = array(
-					'AttachmentName' => basename($filename),
-					'AttachmentData' => base64_encode($filedata));
-		
-				$request_container[] = new SoapVar($attachment, SOAP_ENC_OBJECT, null, null, 'Attachment');
-			}
-			fclose($file);
-		}
-
-		$params = array();
-		$params[] = new SoapVar($request_container, SOAP_ENC_OBJECT, null, null, 'RequestContainer');
-
-		$response = 
-			$this->soapClient
-				->AddAttachmentRequest(
-					new SoapVar($params, SOAP_ENC_OBJECT));
-
-		if ($debug) {
-			return 
-				'Request : ' . 
-				htmlentities($this->soapClient->__getLastRequest()) . 
-				'Response: <br/>' . 
-				var_dump($response->RequestContainer);
-		} else {
-			return $response->RequestContainer;
-		}
-	}
-
-	// CloseShipmentsRequest
-	// ---------------------
-	// Activates procedure of closing some shipments, indicated by their MBE tracking numbers.
-	//	$internalReferenceId	->	string that will be returned as is
-	//	$masterTrackings is a string or an array of strings
-	//					 containing the MOS of shipments to
-	//					 be closed.
-	public function CloseShipments(
-		$internalReferenceId,
-		$masterTrackings,
-		$debug = false)
-	{
-		$request_container = array();
-		$request_container[] = new SoapVar($this->system, XSD_STRING, null, null, 'SystemType');
-		$request_container[] = new SoapVar($this->credentials, SOAP_ENC_OBJECT, null, null, 'Credentials');
-		$request_container[] = new SoapVar($internalReferenceId, XSD_STRING, null, null, 'InternalReferenceID');
-
-		if (!is_array($masterTrackings)) $masterTrackings = array($masterTrackings);
-
-		foreach($masterTrackings as $masterTracking) {
-			$request_container[] = new SoapVar($masterTracking, XSD_STRING, null, null, 'MasterTrackingsMBE');
-		}
-
-		$params = array();
-		$params[] = new SoapVar($request_container, SOAP_ENC_OBJECT, null, null, 'RequestContainer');
-
-		$response = 
-			$this->soapClient
-				->CloseShipmentsRequest(
-					new SoapVar($params, SOAP_ENC_OBJECT));
-
-		if ($debug) {
-			return 
-				'Request : ' . 
-				htmlentities($this->soapClient->__getLastRequest()) . 
-				'Response: <br/>' . 
-				var_dump($response->RequestContainer);
-		} else {
-			return $response->RequestContainer;
-		}
-	}
-
-	// CustomersListRequest
-	// --------------------
-	// For franchises, get a list of customers of some store.
-	//	$internalReferenceId	->	string that will be returned as is
-	//	$storeID				->	store code number
-	public function CustomersList(
-		$internalReferenceID,
-		$storeID,
-		$debug = false)
-	{	
-		$params = array(
-			'RequestContainer' => array(
-				'Credentials' => $this->credentials,
-				'InternalReferenceID' => $internalReferenceID,
-				'StoreID' => $storeID));
-
-		$response =
-			$this->soapClient
-				->CustomersListRequest($params);
-
-		if ($debug) {
-			return 
-				'Request : ' . 
-				htmlentities($this->soapClient->__getLastRequest()) . 
-				'Response: <br/>' . 
-				var_dump($response->RequestContainer);
-		} else {
-			return $response->RequestContainer;
-		}
-	}
-
-	// DeleteShipments
-	// ---------------
-	// Delete shipments indicated by their MBE tracking numbers.
-	//	$internalReferenceId	->	string that will be returned as is
-	//	$masterTrackings		->	is a string or an array of strings
-	//									containing the MOS of shipments to
-	//									be closed.
-	public function DeleteShipments(
-		$internalReferenceID,
-		$masterTrackings,
-		$debug = false)
-	{
-		$request_container = array();
-		$request_container[] = new SoapVar($this->system, XSD_STRING, null, null, 'SystemType');
-		$request_container[] = new SoapVar($this->credentials, SOAP_ENC_OBJECT, null, null, 'Credentials');
-		$request_container[] = new SoapVar($internalReferenceID, XSD_STRING, null, null, 'InternalReferenceID');
-
-		if (!is_array($masterTrackings)) $masterTrackings = array($masterTrackings);
-
-		foreach($masterTrackings as $masterTracking) {
-			$request_container[] = new SoapVar($masterTracking, XSD_STRING, null, null, 'MasterTrackingsMBE');
-		}
-
-		$params = array();
-		$params[] = new SoapVar($request_container, SOAP_ENC_OBJECT, null, null, 'RequestContainer');
-
-		$response = 
-			$this->soapClient
-				->DeleteShipmentsRequest(
-					new SoapVar($params, SOAP_ENC_OBJECT));
-
-		if ($debug) {
-			return 
-				'Request : ' . 
-				htmlentities($this->soapClient->__getLastRequest()) . 
-				'Response: <br/>' . 
-				var_dump($response->RequestContainer);
-		} else {
-			return $response->RequestContainer;
-		}
-	}
-
-	// ListDepartmentsRequest
-	// ----------------------
-	// List defined departments for indicated customer.
-	//	$internalReferenceId	->	string that will be returned as is.
-	//	$customerID				->	ID of customer as given by MBE franchise.
-	public function ListDepartments(
-		$internalReferenceID,
-		$customerID,
-		$debug = false)
-	{
-		$params = array(
-			'RequestContainer' => array(
-				'Credentials' => $this->credentials,
-				'InternalReferenceID' => $internalReferenceID,
-				'CustomerID' => $customerID));
-
-		$response = 
-			$this->soapClient
-				->ListDepartmentsRequest($params);
-
-		if ($debug) {
-			return 
-				'Request : ' . 
-				htmlentities($this->soapClient->__getLastRequest()) . 
-				'Response: <br/>' . 
-				var_dump($response->RequestContainer);
-		} else {
-			return $response->RequestContainer;
-		}
-	}
-
-	// ManageCustomerGET
-	// -----------------
-	// Makes a call to ManageCustomer with GET action.
-	//	$internalReferenceId	->	string that will be returned as is.
-	//	$customerID				->	id of customer as given by MBE franchise.
-	public function ManageCustomerGET(
-		$internalReferenceID,
-		$customerID,
-		$debug = false)
-	{
-		$params = array(
-			'RequestContainer' => array(
-				'SystemType' => $this->system,
-				'Credentials' => $this->credentials,
-				'InternalReferenceID' => $internalReferenceID,
-				'Action' =>'GET',
-				'CustomerID' => $customerID,
-				'Customer' => ''));
-
-		$response = 
-			$this->soapClient
-				->ManageCustomerRequest($params);
-
-		if ($debug) {
-			return 
-				'Request : ' . 
-				htmlentities($this->soapClient->__getLastRequest()) . 
-				'Response: <br/>' . 
-				var_dump($response->RequestContainer);
-		} else {
-			return $response->RequestContainer;
-		}
-	}
-
-
-	// ManageCustomerINSERT
-	// --------------------
-	// Makes a call to ManageCustomer with INSERT action.
-	//	$internalReferenceId	->	string that will be returned as is.
-	//	$customer				->	object of type Customer with required info.
-	public function ManageCustomerINSERT(
-		$internalReferenceID,
-		$customer,
-		$debug = false)
-	{
-		$params = array(
-			'RequestContainer' => array(
-				'SystemType' => $this->system,
-				'Credentials' => $this->credentials,
-				'InternalReferenceID' => $internalReferenceID,
-				'Action' =>'INSERT',
-				'Customer' => $customer->getParams()));
-
-		$response = 
-			$this->soapClient
-				->ManageCustomerRequest($params);
-
-		if ($debug) {
-			return 
-				'Request : ' . 
-				htmlentities($this->soapClient->__getLastRequest()) . 
-				'Response: <br/>' . 
-				var_dump($response->RequestContainer);
-		} else {
-			return $response->RequestContainer;
-		}
-	}
-
-	// ShipmentRequest
-	// ---------------
-	//	$internalReferenceId	->	string that will be returned as is.
-	//	$recipient				->	Object of type Recipient with required info.
-	//	$shipment				->	Object of type Shipment with required info.
-	public function Shipment(
-		$internalReferenceID,
-		$recipient,
-		$shipment,
-		$debug = false)
-	{
-		$params = array(
-			'RequestContainer' => array(
-				'System' => $this->system,
-				'Credentials' => $this->credentials,
-				'InternalReferenceID' => $internalReferenceID,
-				'Recipient' => $recipient->getParams(),
-				'Shipment' => $shipment->getParams()));
-
-		$response = 
-			$this->soapClient
-				->ShipmentRequest($params);
-
-		if ($debug) {
-			return 
-				'Request : ' . 
-				htmlentities($this->soapClient->__getLastRequest()) . 
-				'Response: <br/>' . 
-				var_dump($response->RequestContainer);
-		} else {
-			return $response->RequestContainer;
-		}
-	}
-
-	// ShippingOptionsRequest
-	// ----------------------
-	// Gets shipping options for an indicated shipment parameters.
-	//	$internalReferenceID	->	string that is returned as is by the server.
-	//	$shippingParameters		->	object of type ShippingParameters with all
-	//									the required info.
-	public function ShippingOptions(
-		$internalReferenceID,
-		$shippingParameters,
-		$debug = false)
-	{
-		$params = array(
-			'RequestContainer' =>array(
-				'System' => $this->system,
-				'Credentials' => $this->credentials,
-				'InternalReferenceID' => $internalReferenceID,
-				'ShippingParameters' => $shippingParameters->getParams()));
-
-		$response = 
-			$this->soapClient
-				->ShippingOptionsRequest($params);
-
-		if ($debug) {
-			return 
-				'Request : ' . 
-				htmlentities($this->soapClient->__getLastRequest()) . 
-				'Response: <br/>' . 
-				var_dump($response->RequestContainer);
-		} else {
-			return $response->RequestContainer;
-		}
-
-	}
-
-	// TrackingRequest
-	// ---------------
-	// Gets tracking information from a shipment.
-	//	$internalReferenceID	->	string for reference returned as is.
-	//	$trackingMBE			->	MBE tracking number off the shipment to
-	//									get tracking info about.
-	public function Tracking(
-		$internalReferenceID,
-		$trackingMBE,
-		$debug = false
-	) {
-		$params = array(
-			'RequestContainer' => array(
-				'System' => $this->system,
-				'Credentials' => $this->credentials,
-				'InternalReferenceID' => $internalReferenceID,
-				'TrackingMBE' => $trackingMBE));
-
-		$response =
-			$this->soapClient
-				->TrackingRequest($params);
-
-		if ($debug) {
-			return
-				'Request : ' .
-				htmlentitites($this->soapClient->__getLastRequest()) .
-				'Response: <br/>' .
-				var_dump($response->RequestContainer);
-		} else {
-			return $response->RequestContainer;
-		}
-	}
-}
-=cut
-
 }
 
 1;
--- mbetest.pl
+++ mbetest.pl
@@ -2,19 +2,10 @@
 
 
 
-    # use strict;
-    # use warnings;
-    # use 5.010;
-     
-    # use LWP::UserAgent;
-    # sub LWP::UserAgent::get_basic_credentials {
-    #     warn "@_\n";
-    # }
-    # my $ua = LWP::UserAgent->new;
-    # my $resp = $ua->get( 'https://api.mbeonline.it/ws' );
-
 use lib './lib';
 use Net::MBE;
+use Net::MBE::DestinationInfo;
+use Net::MBE::ShippingParameters;
 use Arthas::Defaults::536;
 
 my $mbe = Net::MBE->new({
@@ -23,6 +14,27 @@ my $mbe = Net::MBE->new({
     Passphrase => 'kGEJ5HzzwjkeMi8pudpLaWD7fX7dn4gZ',
 });
 
-my $response = $mbe->ShippingOptions({
-    InternalReferenceID => '48147184XTST',
+my $dest = Net::MBE::DestinationInfo->new({
+    zipCode => '33085',
+    country => 'IT', 
+    state => 'PN'
 });
+
+my $shipparams = Net::MBE::ShippingParameters->new({
+    destinationInfo => $dest,
+    shipType => 'EXPORT',
+    packageType => 'GENERIC',
+});
+$shipparams->addItem({
+    weight => 1,
+    length => 10,
+    width  => 10,
+    height => 10,
+});
+
+my $response = $mbe->ShippingOptions({
+    internalReferenceID => '48147184XTST',
+    shippingParameters => $shipparams,
+});
+
+use Data::Dump qw/dump/; die dump($response);
