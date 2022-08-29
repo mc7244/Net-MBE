7ccdd66a624c28f49e055482fe54e67fcc8edb95
Michele Beltrame <mb@italpro.net>
2022-08-29 16:19:57 +0200

    yuk

--- lib/Net/MBE.pm
+++ lib/Net/MBE.pm
@@ -1,61 +1,3 @@
-=head1 NAME
-
-Net::MBE - Perl library to access Mailboxes Etc (MBE) online webservices.
-
-=head1 SYNOPSIS
-
-    use Net::MBE;
-
-    my $mbe = Net::MBE->new({
-        system => 'IT',
-        username => 'XXXXX',
-        passphrase => 'YYYYYYYY',
-    });
-
-    my $dest = Net::MBE::DestinationInfo->new({
-        zipCode => '33085',
-        country => 'IT', 
-        state => 'PN'
-    });
-
-    my $shipparams = Net::MBE::ShippingParameters->new({
-        destinationInfo => $dest,
-        shipType => 'EXPORT',
-        packageType => 'GENERIC',
-    });
-    $shipparams->addItem({
-        weight => 1,
-        length => 10,
-        width  => 10,
-        height => 10,
-    });
-
-    my $response = $mbe->ShippingOptions({
-        internalReferenceID => '48147184XTST',
-        shippingParameters => $shipparams,
-    });
-
-    use Data::Dump qw/dump/; print dump($response);
-
-=head1 DESCRIPTION
-
-Mailboxes Etc (MBE), formerly a UPS-owned chain of shipping service outlets, is now an Italian
-independent company which operates in several european countries.
-
-This library is for accessing their various web services for getting rates, etc.
-
-Currently, ONLY getting shipping rates is implemented.
-
-=head1 AUTHOR
-
-Michele Beltrame, C<arthas@cpan.org>
-
-=head1 LICENSE
-
-This library is free software under the Mozilla Public License 2.0.
-
-=cut
-
 package Net::MBE {
     use Moo;
     use namespace::clean;
@@ -189,3 +131,88 @@ package Net::MBE {
 }
 
 1;
+
+=head1 NAME
+
+Net::MBE - Perl library to access Mailboxes Etc (MBE) online webservices.
+
+=head1 SYNOPSIS
+
+    use Net::MBE;
+
+    my $mbe = Net::MBE->new({
+        system => 'IT',
+        username => 'XXXXX',
+        passphrase => 'YYYYYYYY',
+    });
+
+    my $dest = Net::MBE::DestinationInfo->new({
+        zipCode => '33085',
+        country => 'IT', 
+        state => 'PN'
+    });
+
+    my $shipparams = Net::MBE::ShippingParameters->new({
+        destinationInfo => $dest,
+        shipType => 'EXPORT',
+        packageType => 'GENERIC',
+    });
+    $shipparams->addItem({
+        weight => 1,
+        length => 10,
+        width  => 10,
+        height => 10,
+    });
+
+    my $response = $mbe->ShippingOptions({
+        internalReferenceID => '48147184XTST',
+        shippingParameters => $shipparams,
+    });
+
+    use Data::Dump qw/dump/; print dump($response);
+
+=head1 DESCRIPTION
+
+Mailboxes Etc (MBE), formerly a UPS-owned chain of shipping service outlets, is now an Italian
+independent company which operates in several european countries.
+
+This library is for accessing their various web services for getting rates, etc.
+
+Currently, ONLY getting shipping rates is implemented.
+
+=head1 METHODS
+
+=head2 new($args)
+
+Constructs the object. It accepts (and requires) 3 arguments.
+
+    my $mbe = Net::MBE->new({
+        system     => 'IT',         # 2-eltters ISO code of the country where you signed the agreement
+        username   => 'XXXXX',      # Username provided after signing
+        passphrase => 'YYYYYYYY',   # Password provided after signing
+    });
+
+=head2 ShippingOptions($args)
+
+Get shipping options (i.e. rates) for sending a package. Re quires 2 paramenters:
+
+    my $response = $mbe->ShippingOptions({
+        internalReferenceID => 'ORDER_CODE_OR_OTHER_THING',
+        shippingParameters  => $shipparams,
+    });
+
+C<$shipparams> is a L<Net::MBE::ShippingParameters> object.
+
+=head2 Shipment($args)
+
+Create a shipment, so that the van passes to pick it up.
+
+=head1 AUTHOR
+
+Michele Beltrame, C<arthas@cpan.org>
+
+=head1 LICENSE
+
+This library is free software under the Mozilla Public License 2.0.
+
+=cut
